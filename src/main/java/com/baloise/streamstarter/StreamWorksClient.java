143eb84bd227036fbc8cfca13c3b85b764c5ef94
B013331 <peter.bracher@baloise.com>
2019-01-28 16:27:38 +0100

    immediately start for prepared Stream

--- src/main/java/com/baloise/streamstarter/StreamWorksClient.java
+++ src/main/java/com/baloise/streamstarter/StreamWorksClient.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.http.auth.UsernamePasswordCredentials;
 
+import com.baloise.streamstarter.model.ImmedRequest;
 import com.baloise.streamstarter.model.PrepareRequest;
 import com.baloise.streamstarter.model.PrepareResponse;
 import com.baloise.streamstarter.model.Response;
@@ -32,7 +33,9 @@ public class StreamWorksClient {
 		initUniRest();
 	}
 
-
+	/**
+	 * Init Unirest API
+	 */
 	private void initUniRest() {
 		Unirest.setObjectMapper(new ObjectMapper() {
 		    private com.fasterxml.jackson.databind.ObjectMapper jacksonObjectMapper
@@ -61,6 +64,15 @@ public class StreamWorksClient {
 		
 	}
 
+	/**
+	 * Get Stream informations with streamName, planDate and runNumber
+	 * 
+	 * @param streamName
+	 * @param planDate
+	 * @param runNo
+	 * @return
+	 * @throws UnirestException
+	 */
 	public Response getStreamRuns(String streamName, String planDate, int runNo) throws UnirestException {
 		
 		String query = "streamruns";
@@ -76,6 +88,13 @@ public class StreamWorksClient {
 
 	}
 	
+	/**
+	 * Get stream informations with preparatioonId 
+	 * 
+	 * @param id
+	 * @return
+	 * @throws UnirestException
+	 */
 	public Response getStreamRunsPrepared(String id) throws UnirestException {
 		
 		String query = "streamruns";
@@ -89,7 +108,15 @@ public class StreamWorksClient {
 		return request(req, Response.class);
 	}
 	
-
+	/**
+	 * Get Job informations with streamname planDate runNumber
+	 * 
+	 * @param streamName
+	 * @param planDate
+	 * @param runNo
+	 * @return
+	 * @throws UnirestException
+	 */
 	public Response getStreamRunJobs(String streamName, String planDate, int runNo) throws UnirestException {
 		
 		String query = "streamrunjobs";
@@ -105,6 +132,13 @@ public class StreamWorksClient {
 		return request(req, Response.class);
 	}
 	
+	/**
+	 * Get Job informations from Stream with preparationId
+	 * 
+	 * @param id
+	 * @return 
+	 * @throws UnirestException
+	 */
 	public Response getStreamRunJobsPrepared(String id) throws UnirestException {
 		
 		String query = "streamrunjobs";
@@ -117,7 +151,14 @@ public class StreamWorksClient {
 		
 		return request(req, Response.class);
 	}
-	
+	/**
+	 * Schedule Stream
+	 * 
+	 * @param streamName
+	 * @param planDate
+	 * @return Status 
+	 * @throws UnirestException
+	 */
 	public int schedule(String streamName, String planDate) throws UnirestException {
 		
 		String query = "schedules";
@@ -136,8 +177,39 @@ public class StreamWorksClient {
 		
 		return response.getStatus();				
 	}
+	/**
+	 * Start Stream immediately 
+	 * if start-time not set by streamworks  or start-time is later than current time
+	 *     
+ 	 * @return
+	 * @throws UnirestException
+	 */
+	public int streamrunstartsPrepared(String prepId, ImmedRequest request) throws UnirestException {
+		
+		String query = "streamrunstarts";		 
+			
+		String url = baseURL+"{query}/{mandator}/{prepId}";
+		
+		String body = request.toString();
+		// System.out.println(body);
+		BaseRequest req = Unirest.post(url)
+				  .routeParam("query", query)
+				  .routeParam("mandator", mandator)
+				  .routeParam("prepId", prepId)
+				  .basicAuth(credentials.getUserName(), credentials.getPassword())
+				  .header("accept", "text/plain")
+				  .header("Content-Type" , "application/json") 
+				  .body(body);
+
+		System.err.println(req.asString().getBody());		
+		HttpResponse<String> response = req.asString();
+		
+		return response.getStatus();
+	}
 	
 	/**
+	 * Prepare Stream next run with Parameters
+	 * 
 	 * @return the preparationId
 	 */
 	public String prepareNextWithParms(String streamName, String planDate, PrepareRequest request) throws UnirestException {
@@ -167,6 +239,8 @@ public class StreamWorksClient {
 	}
 
 	/**
+	 * Prepare Stream
+	 * 
 	 * @return the preparationId
 	 */
 	public String prepareNext(String streamName, String planDate) throws UnirestException {
@@ -194,6 +268,14 @@ public class StreamWorksClient {
 		return response.getBody().PreparationIds.P;
 	}
 	
+	/**
+	 * Response
+	 * 
+	 * @param req
+	 * @param responseClass
+	 * @return
+	 * @throws UnirestException
+	 */
 	private Response request(HttpRequest req, Class<Response> responseClass) throws UnirestException {
 		req.header("accept", "application/json")
 		.basicAuth(credentials.getUserName(), credentials.getPassword());
